a9637165712801cc9a250538005e388c47af7e82
sevndl <nandillons@gmail.com>
2021-03-16 14:57:26 +0100

    Communication de données locales côté serveur vers le client

--- ws-rendu/server/main.js
+++ ws-rendu/server/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+import { localDatas } from './local-data.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(() => {});
+
+WebApp.connectHandlers.use('/api/discover/movies', (req, res, next) => {
+  res.writeHead(200);
+  res.end(JSON.stringify(localDatas));
 });
